67b5c97fd71c3524ccfe0971d8749d1681d8cdee
PradipH31 <pradip.hamal31@gmail.com>
2023-08-15 12:25:27 -0500

    Added Events for Orders

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/pradiph31/SpeedyDine/order/service/domain/event/OrderCancelledEvent.java
@@ -0,0 +1,12 @@
+package com.pradiph31.SpeedyDine.order.service.domain.event;
+
+import com.pradiph31.SpeedyDine.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderCancelledEvent extends OrderEvent {
+
+    public OrderCancelledEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/pradiph31/SpeedyDine/order/service/domain/event/OrderPaidEvent.java
@@ -0,0 +1,12 @@
+package com.pradiph31.SpeedyDine.order.service.domain.event;
+
+import com.pradiph31.SpeedyDine.order.service.domain.entity.Order;
+
+import java.time.ZonedDateTime;
+
+public class OrderPaidEvent extends OrderEvent {
+
+    public OrderPaidEvent(Order order, ZonedDateTime createdAt) {
+        super(order, createdAt);
+    }
+}
